a7448aa29dba3916ee22c7d2bc031f8a1b1c7a02
coderaiser <coderaiser@cloudcmd.io>
2016-12-17 14:31:35 +0200

    feature(package) eachSeries -> itchy

--- bin/tar-to-zip.js
+++ bin/tar-to-zip.js
@@ -12,7 +12,7 @@ if (/^(-v|--version)$/.test(arg))
 else if (!arg && isTTY || /^(-h|--help)$/.test(arg))
     help();
 else if (args.length)
-    eachSeries(args, main, exitIfError);
+    require('itchy')(args, main, exitIfError);
 else
     require('..')(process.stdin)
         .getStream()
@@ -93,34 +93,5 @@ function help() {
     Object.keys(bin).forEach((name) => {
         console.log(`  ${name} ${bin[name]}`);
     });
-}
-
-function eachSeries(array, iterator, done) {
-    check(array, iterator, done);
-    
-    let i = -1;
-    const n = array.length;
-    
-    const loop = (e) => {
-       ++i;
-       
-       if (e || i === n)
-           return done(e);
-       
-       iterator(array[i], loop);
-    };
-    
-    loop();
-}
-
-function check(array, iterator, done) {
-    if (!Array.isArray(array))
-        throw Error('array should be an array!');
-    
-    if (typeof iterator !== 'function')
-        throw Error('iterator should be a function');
-    
-    if (typeof done !== 'function')
-        throw Error('done should be a function');
 }
 
